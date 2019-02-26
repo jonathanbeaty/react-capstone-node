970bca49ed5837bbf768cebfb118aa306050df4f
Jonathan Beaty <jonathanbeaty15@gmail.com>
2019-02-26 06:10:05 -0600

    updating

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://test123:test123@ds141815.mlab.com:41815/react-capstone' || 'mongodb://localhost/react-capstone';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/react-capstone';
-exports.API_BASE_URL = process.env.REACT_APP_API_ORIGIN || 'https://react-capstone-jb.herokuapp.com/'
+exports.API_BASE_URL = process.env.REACT_APP_API_ORIGIN || 'https://immense-mountain-22076.herokuapp.com/'
 exports.PORT = process.env.PORT || 8080;
 exports.JWT_SECRET = process.env.JWT_SECRET;
 exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
